4659d1a4314ddf4b18e62c7734d6108e7128bfc3
Olga Ivanova <olga.iva@gmail.com>
2020-06-15 15:54:44 +0200

    working on part 2

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const multer = require("multer"); //npm pkg for handling multipart/form data on server side, adds the file and the body to the request object
+const uidSafe = require("uid-safe"); // npm pkg that generates random and unique string
+const path = require("path"); // core module, helps with handling files by providing path
 
 app.use(express.static("./public"));
+app.use(express.json());
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.get("/images", (req, res) => {
     db.getImages()
@@ -14,5 +36,17 @@ app.get("/images", (req, res) => {
             console.log("err in getImages:", err);
         });
 });
-
+// uploader triggers  multer boilerplate that handles that files are being stored on  harddisk,
+//in the uploads folder, single is a method of uploader
+//'file' comes from the property that was  set on formData
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("file:", req.file);
+    console.log("req.body", req.body);
+    if (req.file) {
+        // we will eventually want to make a db insert query where we insert all image information into our database, but before doing this we need to talk about how to get our images up into the cloud
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
 app.listen(8080, () => console.log("imageboard server up and running"));
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,64 @@
-console.log("sanity check");
+(function () {
+    console.log("sanity check");
 
-new Vue({
-    el: "#main",
-    data: {
-        images: [],
-    },
-    mounted: function () {
-        var self = this;
-        axios
-            .get("/images")
-            .then(function (response) {
-                console.log("response.data: ", response.data);
-                self.images = response.data;
-            })
-            .catch((err) => {
-                console.log("err: ", err);
-            });
-    },
-});
+    new Vue({
+        el: "#main",
+        data: {
+            images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
+        },
+        mounted: function () {
+            var self = this;
+            axios
+                .get("/images")
+                .then(function (response) {
+                    console.log("response.data: ", response.data);
+                    self.images = response.data;
+                })
+                .catch((err) => {
+                    console.log("err: ", err);
+                });
+        },
+
+        methods: {
+            handleClick: function (e) {
+                console.log("clicked submit  button!");
+                // prevent refresh from running on button click:
+                e.preventDefault();
+                // whatever code I write will run whenever the user clicks the submit btn
+                // to see what is in my data object of the vue instance log this
+                console.log("this:", this);
+                // we NEED to use FormData to send a file to the server
+                var formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+                // formData has this strange behavior that if I console.log it at this point it will log an empty object!!! You have to loop through the values of the object in a specific way if you want to see them client side.
+                var thisOfData = this;
+                axios
+                    .post("/upload", formData)
+                    .then(function (resp) {
+                        console.log("resp from POST /upload:", resp);
+                        thisOfData.title = thisOfData.description = thisOfData.username =
+                            "";
+                        thisOfData.file = null;
+                    })
+                    .catch(function (err) {
+                        console.log("err in POST /upload:", err);
+                    });
+            },
+            handleChange: function (e) {
+                console.log("handleChange ran");
+                // our eventobject has access to the file that was put the input field, aka selected for upload
+                console.log("file:", e.target.files[0]);
+                // make sure that our file property of our data object holds the value of our file in the input field
+                this.file = e.target.files[0];
+                // we are using FormData because we want to send files along in our request to the server!
+            },
+        },
+    });
+})();
